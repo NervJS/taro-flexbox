86e67dd83ad2e9b7cdc6754c6b28f324bb9206c7
zhutianjian <zhutianjian@ECCMAC-C02XFG0W.local>
2019-09-06 10:56:51 +0800

    fix: swan

--- flexbox-demo/src/components/flexDemo/index.tsx
+++ flexbox-demo/src/components/flexDemo/index.tsx
@@ -10,7 +10,7 @@ function RenderPanel(props: PanelProps): JSX.Element {
   return <Panel 
     classes={{ title: clnx(['z-index2', 'top0', 'black-bg8', 'padding1'], process.env.TARO_ENV === 'rn' ? 'height2_5' : 'height2'), content: clnx('padding2') }}
     renderTitle={
-      process.env.TARO_ENV === 'rn' ?
+      process.env.TARO_ENV !== 'h5' ?
       <Text className={clnx(['font-size1_25', 'line-height2', 'white', 'bolder'])}>{props.title}</Text> :
       <h2 id={`#${props.key}`}><a href={`#${props.key}`}><Text className={clnx(['font-size1_25', 'line-height2', 'white', 'bolder'])}>{props.title}</Text></a></h2>
     }>
@@ -19,7 +19,7 @@ function RenderPanel(props: PanelProps): JSX.Element {
         key={e.key}
         classes={{ title: clnx(['z-index1', 'black-bg2', 'padding1'], process.env.TARO_ENV !== 'rn' ? ['top98'] : []), content: clnx('padding2') }}
         renderTitle={
-          process.env.TARO_ENV === 'rn' ? <View>
+          process.env.TARO_ENV !== 'h5' ? <View>
             <View>
               <Text className={clnx(['font-size1', 'cream', 'bolder'])}>{e.title}</Text>
             </View>
